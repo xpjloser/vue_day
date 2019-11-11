9143383d40d8d4841f0f767159c030a6c08cf1b7
熊培京 <2089977690@qq.com>
2019-11-11 20:32:03 +0800

    第一次vue项目

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,81 +1,132 @@
 <template>
-    <div class="login_dise">
-        <div class="login_box">
-            <!-- 头像区域 -->
-            <div class="user">
-                <img src="../assets/logo.png" alt="">
-            </div>
-            <!-- 登陆表单区 -->
-            <el-form  label-width="0px" class="login_form">
-                <!-- 用户名 -->
-                <el-form-item >
-                    <el-input prefix-icon="iconfont icon-user"></el-input>
-                </el-form-item>
-                <!-- 密码 -->
-                <el-form-item >
-                    <el-input prefix-icon="iconfont icon-3702mima"></el-input>
-                </el-form-item>
-                <!-- 按钮 -->
-                <el-form-item class="btns">
-                    <el-button type="primary" round>登录</el-button>   
-                    <el-button type="info" round>注销</el-button> 
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="login_dise">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="user">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登陆表单区 -->
+      <el-form
+        ref="loginFormRef"
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在3到10个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在6到15个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    resetLoginForm() {
+      console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录密码错误')
+        this.$message.success('登录成功')
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 通过编程式导航跳转到后台主页，路由地址/home
+        this.$router.push('/home')
+      })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-    .login_dise{
-        position: relative;
-        background-color: skyblue;
-        height: 100%;
+.login_dise {
+  position: relative;
+  background-color: skyblue;
+  height: 100%;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 25px;
+  box-sizing: border-box;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .user {
+    overflow: hidden;
+    width: 130px;
+    height: 130px;
+    background-color: #fff;
+    border: 1px solid #fff;
+    padding: 10px;
+    box-shadow: 0, 0, 10px, #ddd;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    top: 0;
+    transform: translate(-50%, -50%);
+    img {
+      width: 100%;
+      height: 100%;
+      background-color: #eee;
+      border-radius: 50%;
     }
-    .login_form{
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        padding: 25px;
-        box-sizing: border-box;
-    }
-    .login_box{
-        width: 450px;
-        height: 300px;
-        background-color: #fff;
-        border-radius: 10px;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,-50%);
-        .user{
-            overflow: hidden;
-            width: 130px;
-            height: 130px;
-            background-color: #fff;
-            border:1px solid #fff;
-            padding: 10px;
-            box-shadow: 0,0,10px,#ddd;
-            border-radius: 50%;
-            position: absolute;
-                left: 50%;
-                top: 0;
-                transform: translate(-50%,-50%);
-            img{
-                width: 100%;
-                height: 100%;
-                background-color: #eee;
-                border-radius: 50%;
-                
-            }
-        }
-    }
-    .btns{
-        text-align: right;
-    }
+  }
+}
+.btns {
+  text-align: right;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from './components/login.vue'
+import home from './components/home.vue'
+
 
 
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
     routes: [
         { path: '/', redirect: '/login' },
+        { path: '/login', component: login },
+        {
+            path:'/home',
+            component:home,
+            // redirect:'/welcome',
+            // children:[
+            //     {path:'/welcome',component:Welcome},
+            //     {path:'/users',component:Users}
 
-        { path: '/login', component: login }
+            // ]
+        }
     ]
 })
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    // to 将要访问的路径
+    // from 代表从哪个路径跳转而来
+    // next 是一个函数，表示放行
+    //     next()  放行    next('/login')  强制跳转
+  
+    if (to.path === '/login') return next()
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+  })
+  
+  export default router
